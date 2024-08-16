4443828c113a606658cedeba0269c42d2e35bb7d
EC2 Default User <ec2-user@ip-172-31-12-187.ap-northeast-1.compute.internal>
2024-08-16 13:51:37 +0000

    firstcommit

---
+++ sports/cricke.sh
@@ -0,0 +1,9 @@
+cricket is the game played by 11 players
+it is invented by englaand
+but it most populin india
+some most popular players in our country
+viratkohli
+#sehwag
+hardik
+echo "gambhir"
+"
---
+++ sports/kabaddi.java
@@ -0,0 +1,9 @@
+kabaddi is played by 7 players
+and this game was invented by india
+this is alsso most popular in countries like india japan austrlia england pak .etc
+the most popular players in india are
+pardeep
+rahul
+nivas
+pavan
+
