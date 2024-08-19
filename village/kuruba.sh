7958264ff1e2ae41f108586675e478bd8856898a
EC2 Default User <ec2-user@ip-172-31-12-187.ap-northeast-1.compute.internal>
2024-08-19 03:32:56 +0000

    in second

---
+++ village/kuruba.sh
@@ -0,0 +1,3 @@
+thw name kuruba is a caste name 
+mostly presented in the karntk  nd ndhrapradesh stated like anantapur and satya sai districts 
+
---
+++ village/reddy.java
@@ -0,0 +1,4 @@
+this is the caste with having most ricjest peop;e inthis catsre 
+most of my friends are reddys 
+they are having more mponey
+i dont like this caste people
